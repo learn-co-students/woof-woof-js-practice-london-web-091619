bde94cb4bd26f014639dca8ccf6a9babff254afa
thetamicodes <reichold.tamara@gmail.com>
2019-11-07 10:05:21 +0000

    everything but bonus (filter)

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
-const dogsUrl = 'http://localhost:3000/pups'
-
 document.addEventListener('DOMContentLoaded', function() {
+
+    const dogsUrl = 'http://localhost:3000/pups'
+
     fetchDogs();
     dataDogs();
     filterDogs();
 
     function fetchDogs() {
-        return fetch('http://localhost:3000/pups')
+        return fetch(dogsUrl)
         .then(function(response) {
             return response.json()
         })
@@ -15,21 +16,25 @@ document.addEventListener('DOMContentLoaded', function() {
     function dataDogs() {
         fetchDogs()
         .then(function(dogs) {
-            renderDogsBar(dogs)
+            singleDog(dogs)
         })
     }
+
+    function singleDog(dogs) {
+        for (let i = 0; i < dogs.length; i++) {
+            renderDogsBar(dogs[i])
+        }
+    }
     
-    function renderDogsBar(dogs) {
+    function renderDogsBar(dog) {
         const dogBar = document.getElementById('dog-bar');
-        dogs.forEach(function(dog) {
-            const dogElement = document.createElement('span');
-            dogElement.id = `span${dog.id}`;
-            dogElement.innerHTML = `${dog.name}`;
-            dogBar.appendChild(dogElement);
+        const dogElement = document.createElement('span');
+        dogElement.id = `span${dog.id}`;
+        dogElement.innerHTML = `${dog.name}`;
+        dogBar.appendChild(dogElement);
         
-            dogElement.addEventListener('click', function(e) {
-                renderDoggo(dog);
-            })
+        dogElement.addEventListener('click', function(e) {
+            renderDoggo(dog);
         })
     }
     
@@ -53,12 +58,11 @@ document.addEventListener('DOMContentLoaded', function() {
     }
     
     function updateDoggo(dog) {
-        let goodBadButton = document.getElementById(`dog-btn${dog.id}`).innerText;
-        if (goodBadButton == "Good Dog!") {
-            goodBadButton = "Bad Dog!"
+        if (dog.isGoodDog == true) {
+            dog.isGoodDog = false
             goodBad = false
-        } else if (goodBadButton == "Bad Dog!") {
-            goodBadButton = "Good Dog!"
+        } else if (dog.isGoodDog == false) {
+            dog.isGoodDog = true
             goodBad = true
         }
     
@@ -73,7 +77,7 @@ document.addEventListener('DOMContentLoaded', function() {
             })
         }
     
-        fetch(`http://localhost:3000/pups/${dog.id}`, configObject)
+        fetch(`${dogsUrl}/${dog.id}`, configObject)
         .then(function(response) {
             return response.json()
         })
@@ -82,24 +86,24 @@ document.addEventListener('DOMContentLoaded', function() {
         })
     }
     
-    function filterDogs() {
-        const dogFilter = document.getElementById('good-dog-filter');
-        dogFilter.addEventListener('click', function(e) {
-            if (dogFilter.innerText == "Filter good dogs: OFF") {
-                dogFilter.innerText = "Filter good dogs: ON";
-                fetchDogs()
-                .then(function(dogs) {
-                    renderDogsBar(dogs.filter(function(dog) { dog.isGoodDog == true}))
-                })
-            } else if (dogFilter.innerText == "Filter good dogs: ON") {
-                dogFilter.innerText = "Filter good dogs: OFF"
-                fetchDogs()
-                .then(function(dogs) {
-                    renderDogsBar(dogs)
-                })
-            }
-        })
-    }
+    // function filterDogs() {
+    //     const dogFilter = document.getElementById('good-dog-filter');
+    //     dogFilter.addEventListener('click', function(e) {
+    //         if (dogFilter.innerText == "Filter good dogs: OFF") {
+    //             dogFilter.innerText = "Filter good dogs: ON";
+    //             fetchDogs()
+    //             .then(function(dogs) {
+    //                 singleDog(dogs.filter(function(dog) { dog.isGoodDog == true}))
+    //             })
+    //         } else if (dogFilter.innerText == "Filter good dogs: ON") {
+    //             dogFilter.innerText = "Filter good dogs: OFF"
+    //             fetchDogs()
+    //             .then(function(dogs) {
+    //                 singleDog(dogs)
+    //             })
+    //         }
+    //     })
+    // }
 
 })
 
